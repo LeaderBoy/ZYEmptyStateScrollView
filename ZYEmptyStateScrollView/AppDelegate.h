baf9f30e27807f50988beb17d9878b333052e46e
LeaderBoy <748603349@qq.com>
2017-12-11 12:03:55 +0800

    Initial Commit

---
+++ ZYEmptyStateScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYEmptyStateScrollView
+//
+//  Created by 杨志远 on 2017/12/11.
+//  Copyright © 2017年 BaQiWL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYEmptyStateScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYEmptyStateScrollView
+//
+//  Created by 杨志远 on 2017/12/11.
+//  Copyright © 2017年 BaQiWL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
